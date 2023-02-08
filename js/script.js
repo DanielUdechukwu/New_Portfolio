c3f9428588ae7db474688a2bceb171ebf7235eb9
Daniel <danieludechukwu117@gmail.com>
2023-02-08 15:37:48 +0100

    the beningin

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const Type = (document.getElementById('circle'));
+
+const rotate = new CircleType(Type);
+
+
+let date = document.getElementById("year");
+
+let month = new Date().getFullYear();
+
+date.innerHTML = month;
