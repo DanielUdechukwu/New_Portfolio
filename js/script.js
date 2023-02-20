bde75d3ab1d3e5de49893fea513a4d022b61dd9b
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-02-20 01:54:48 +0100

    Modifications

--- js/script.js
+++ js/script.js
@@ -9,3 +9,30 @@ let date = document.getElementById("year");
 let month = new Date().getFullYear();
 
 date.innerHTML = month;
+
+
+function resetForm() {
+  document.getElementById("form").reset();
+}
+
+const btn = document.getElementById('form-button');
+
+document.getElementById('form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    btn.innerHTML = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_uvytpta';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.innerHTML = 'Send Message';
+        alert('Your message has been sent!');
+        resetForm();
+      }, (err) => {
+        btn.innerHTML = 'Send Message';
+        alert(JSON.stringify(err));
+      });
+  });
